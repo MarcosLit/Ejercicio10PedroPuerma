27e66dfd0f1b023f038b73884a5b89134ecc309d
MarcosLit <marped273@alu.centroafuera.es>
2023-01-31 12:31:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio10pedropuerma/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.ejercicio10pedropuerma
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.ejercicio10pedropuerma.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.imageButton.setOnClickListener(){
+            val numeros = 1..4
+            var numero = numeros.random().toString()
+            var cambio = Intent(this, MainActivity::class.java)
+
+            when(numero){
+                "1"->cambio=Intent(this, Ciudad::class.java);
+                "2"->cambio=Intent(this, Enemigo::class.java);
+                "3"->cambio=Intent(this, Mercader::class.java);
+                "4"->cambio=Intent(this, Objeto::class.java);
+            }
+            startActivity(cambio)
+        }
+    }
+}
